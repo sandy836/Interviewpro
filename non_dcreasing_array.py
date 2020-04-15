ffe6e5d33e077758fd7a0d11cdbabb981d207cda
sandy836 <sandeepshrivastava518@gmail.com>
2020-04-15 00:07:57 -0700

    ono decreasing sequence

---
+++ non_dcreasing_array.py
@@ -0,0 +1,24 @@
+#Time complexity O(nlogn) and Space complexity O(1)
+def check_1(nums):
+    copy_1, copy_2 = nums[:], nums[:]
+    for i in range(0, len(nums)-1):
+        if nums[i]>nums[i+1]:
+            copy_1[i] = nums[i+1]
+            copy_2[i+1] = nums[i]
+            break
+    return copy_1 == sorted(copy_1) or copy_2 == sorted(copy_2)
+
+#Time complexity O(n) and Space complexity O(1)
+def check_2(nums):
+    count = 0
+    for i in range(1, len(nums)):
+        if nums[i-1]>nums[i]:
+            count += 1
+            if i-2<0 and nums[i-2]<nums[i]:
+                nums[i-1] = nums[i]
+            else:
+                nums[i] = nums[i-1]
+    return count<=1
+
+print(check_1([13, 4, 7]))
+print(check_2([5,1,3,2,5]))
